9e9a955c6c77dfea0b9156962d111bd2ce80f420
JayMgitM <mowohbaar@yahoo.com>
2024-08-03 18:59:43 -0500

    first commit

---
+++ src/main/java/com/baar/sample/SampleApplication.java
@@ -0,0 +1,20 @@
+package com.baar.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SampleApplication {
+	@GetMapping("/")
+	public String hello() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApplication.class, args);
+	}
+
+}
