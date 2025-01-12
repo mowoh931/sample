0ba68b4e3e231d21d094a54750ff512757be4c5e
JayMgitM <mowohbaar@yahoo.com>
2025-01-12 17:42:43 -0600

    update

--- src/main/java/com/baar/sample/SampleApplication.java
+++ src/main/java/com/baar/sample/SampleApplication.java
@@ -13,6 +13,11 @@ public class SampleApplication {
 		return "Hello World!";
 	}
 
+	@GetMapping("/test")
+	public String test() {
+		return "Test";
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
 	}
